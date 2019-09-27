09617dfac141b4bb21782c004bcf1ea00354505a
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-09-27 19:35:09 -0400

    Solve bounds problem

--- src/bounds/Main.java
+++ src/bounds/Main.java
@@ -1,8 +1,34 @@
 package bounds;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Bounds Problem");
+        System.out.println("Bounds Problem\n");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int startingNumber = Integer.parseInt(userInput.nextLine());
+
+        System.out.print("Enter the upper bound: ");
+        int upperBound = Integer.parseInt(userInput.nextLine());
+
+        System.out.print("Enter the step size: ");
+        int step = Integer.parseInt(userInput.nextLine());
+
+        System.out.println();
+        System.out.println("Counting from " + startingNumber + " to " + upperBound + " by " + step);
+        System.out.println();
+
+        int currentNumber = startingNumber;
+        int lineCount = 0;
+        while (currentNumber < upperBound) {
+            if (lineCount == 10) {
+                System.out.println();
+                lineCount = 0;
+            }
+            System.out.print(currentNumber + " ");
+            currentNumber = currentNumber + step;
+            lineCount++;
+        }
     }
 }
