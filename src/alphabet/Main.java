36199612a0a00749377c40ae7f9927adda77d9b4
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-09-26 23:54:20 -0400

    Solve alphabet problem

--- src/alphabet/Main.java
+++ src/alphabet/Main.java
@@ -3,6 +3,25 @@ package alphabet;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Alphabet Problem");
+	    System.out.println("Alphabet Problem\n--------------------");
+
+	    char alpha = 'a';
+	    String letterType = "";
+
+	    while (alpha != 'z') {
+	        switch(alpha) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y': letterType = "vowel"; break;
+                default: letterType = "consonant";
+            }
+
+            System.out.println(alpha + " is a " + letterType);
+
+            alpha += 1;
+        }
     }
 }
